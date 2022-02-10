7b49ae4d2a69373e61d3b50c2a69bbe20848e936
Nes-Martinez <nestor.a.martinez@outlook.com>
2022-02-10 12:13:14 -0500

    Added Client React-App

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./server/config/db");
+const path = require("path");
 
 const app = express();
 connectDB();
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.use("/api", require("./api"));
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
