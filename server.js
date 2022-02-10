9b19b9dbaa7b67aab94dcbeab5c902e8e40310b0
Nes-Martinez <nestor.a.martinez@outlook.com>
2022-02-10 12:30:42 -0500

    Removed postinstall script

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.use("/api", require("./api"));
+// app.use("/api", require("./routes"));
 
 app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
