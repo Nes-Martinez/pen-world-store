768becdb651476ecb5195e9b2becba793107b580
Nes-Martinez <nestor.s.martinez@outlook.com>
2022-03-08 10:07:55 -0500

    Added proxy server, salt hash

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -20,6 +21,16 @@ const UserSchema = new mongoose.Schema({
     type: Boolean,
     default: false,
   },
+});
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 const User = mongoose.model("User", UserSchema);
